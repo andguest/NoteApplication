e8c9aef59922e4e341039af739afa2e8d7487555
ctrluserh <harman.saini@mail.utoronto.ca>
2024-11-08 22:50:44 -0500

    Merged and Updated Changes in Weather Entity, Interface and DAO.

--- src/main/java/data_access/WeatherDataAccessObject.java
+++ src/main/java/data_access/WeatherDataAccessObject.java
@@ -4,38 +4,35 @@ import entity.Weather;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
 import org.json.JSONException;
 import org.json.JSONObject;
+import use_case.note.WeatherDataAccessInterface;
+
 import java.io.IOException;
 
 /**
- * This class does stuff.
+ * This class runs the API and creates a weather DAO.
  **/
 
-public class WeatherDataAccessObject {
+public class WeatherDataAccessObject implements WeatherDataAccessInterface {
     private static final String API_KEY = "7cce48d7f1f6785f54c0d08aa117ad83";
+    private static final String MAIN = "main";
     private static String city;
     private static final int SUCCESS_CODE = 200;
-    private static final String CONTENT_TYPE_LABEL = "Content-Type";
     private static final String CONTENT_TYPE_JSON = "application/json";
     private static final String STATUS_CODE_LABEL = "cod";
-    // fixed
     private static final String WEATHER_LIST = "list";
-    private static final String PASSWORD = "password";
     private static final String MESSAGE = "message";
 
-    public WeatherDataAccessObject(String city) {
-        this.city = city;
-    }
-
-    public Weather get(String username) {
+    @Override
+    public Weather getWeather(String city) throws IOException {
         // Make an API call to get the user object.
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
 
         // creating file
         final Request request = new Request.Builder()
-                .url(String.format("http://api.openweathermap.org/data/2.5/forecast?q=%s&appid=" + API_KEY + "&units=metric", city))
+                .url(String.format("http://api.openweathermap.org/data/2.5/forecast?q=%s&appid="
+                        + API_KEY + "&units=metric", city))
                 .addHeader("Content-Type", CONTENT_TYPE_JSON)
                 .build();
         try {
@@ -44,19 +41,25 @@ public class WeatherDataAccessObject {
             final JSONObject responseBody = new JSONObject(response.body().string());
 
             if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
-                final JSONObject userJSONObject = responseBody.getJSONObject(WEATHER_LIST);
+                final JSONObject weatherJSON = responseBody.getJSONArray(WEATHER_LIST).getJSONObject(0);
 
                 // get individual items from the json object
-                // final String name = userJSONObject.getString();
 
-                // return new Weather(name); pass values in
+                final int lat = (int) weatherJSON.getJSONObject(MAIN).getDouble("lat");
+                final int lon = (int) weatherJSON.getJSONObject(MAIN).getDouble("lon");
+                final int temp = (int) weatherJSON.getJSONObject(MAIN).getDouble("temp");
+                final String looks = weatherJSON.getJSONObject("weather").getString(MAIN);
+
+                return new Weather(city, lon, lat, temp, looks);
+
             }
             else {
-                throw new RuntimeException(responseBody.getString(MESSAGE));
+                throw new IOException(responseBody.getString(MESSAGE));
             }
         }
         catch (IOException | JSONException ex) {
-            throw new RuntimeException(ex);
+            throw new IOException(ex);
         }
     }
 }
+
