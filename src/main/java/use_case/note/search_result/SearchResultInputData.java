499cacf1ba46df822972201b31de7aed2c2044e2
linhaoli <lilinhao.li@mail.utoronto.ca>
2024-11-11 21:41:38 -0500

    SearchResultInteractor implemented.

--- src/main/java/use_case/note/search_result/SearchResultInputData.java
+++ src/main/java/use_case/note/search_result/SearchResultInputData.java
@@ -3,13 +3,14 @@ package use_case.note.search_result;
 // Didn't implement the use case for past 24 hours. Undecided between a separate use case or putting it inside
 // search bar.
 public class SearchResultInputData {
-    private final String cityName;
+
+    private static final String city;
 
     public SearchResultInputData(String cityName) {
-        this.cityName = cityName;
+        this.city = cityName;
     }
 
-    public String getCity() {
-        return cityName;
+    public static String getCity() {
+        return city;
     }
 }
