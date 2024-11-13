fe5fe86e74ae4a88426aaa3c695c832e01bdd627
ctrluserh <harman.saini@mail.utoronto.ca>
2024-11-13 11:38:41 -0500

    Created teh Converter and Added setters to Weather

---
+++ src/main/java/entity/Converter.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Converter {
+    public static final double KILOMETERS_MILE = 0.62;
+    public static final double CELC_FAREN = 1.8;
+    public static final double FAREN_ADD = 32
+
+    public Converter(Weather weather) {
+        weather.setWindSpeed((int) (weather.getWindSpeed() * KILOMETERS_MILE));
+        weather.setTemperature((int) ((weather.getTemperature() * CELC_FAREN)+ FAREN_ADD));
+
+    }
+}
