17e2816b247f10ae2a64a747a36e38580731501a
ctrluserh <harman.saini@mail.utoronto.ca>
2024-11-13 11:25:56 -0500

    Created the Input/Output Boundaries and Data

---
+++ src/main/java/use_case/note/convert_farenheit/ConvertFarenheitInputBoundary.java
@@ -0,0 +1,5 @@
+package use_case.note.convert_farenheit;
+
+public interface ConvertFarenheitInputBoundary {
+    void execute(ConvertFarenheitInputData convertFarenheitInputData);
+}
---
+++ src/main/java/use_case/note/convert_farenheit/ConvertFarenheitInputData.java
@@ -0,0 +1,4 @@
+package use_case.note.convert_farenheit;
+
+public class ConvertFarenheitInputData {
+}
