ecd02fa34f375856c79c50ea489800cd61d70d40
Ishayu Sharma <ishayu.sharma@mail.utoronto.ca>
2024-11-16 04:39:16 -0500

    changed weather variable declaration in weatherstate

--- src/main/java/interface_adapter/weather/WeatherState.java
+++ src/main/java/interface_adapter/weather/WeatherState.java
@@ -6,7 +6,7 @@ import entity.Weather;
  * The state information for the weather view model.
  */
 public class WeatherState {
-    private Weather weather = new Weather("", 0, 0, 0, "", "", 0, 0);
+    private Weather weather;
     private String error;
 
     public Weather getWeather() {
