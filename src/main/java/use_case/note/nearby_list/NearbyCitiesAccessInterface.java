2ffaed38886519d4d933dacaac93f9df232a3f53
Ishayu Sharma <ishayu.sharma@mail.utoronto.ca>
2024-11-18 18:22:33 -0500

    Finished interface + object for cities list, added data file for cities

--- src/main/java/use_case/note/nearby_list/NearbyCitiesAccessInterface.java
+++ src/main/java/use_case/note/nearby_list/NearbyCitiesAccessInterface.java
@@ -3,13 +3,9 @@ package use_case.note.nearby_list;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Interface for generating list of nearby cities.
+ */
 public interface NearbyCitiesAccessInterface {
-    /**
-     *
-     * @param latitude
-     * @param longitude
-     * @return
-     * @throws IOException
-     */
     List<String> getNearbyCities(Float latitude, Float longitude) throws IOException;
 }
