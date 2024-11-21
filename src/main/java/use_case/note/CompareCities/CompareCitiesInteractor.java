ba02ff7a79b2de63e192fe401d451b788c9459f5
sophie <sophie.liao@mail.utoronto.ca>
2024-11-21 11:42:12 -0800

    A bit refactor (rename to make the file look consist with other classes)

--- src/main/java/use_case/note/CompareCities/CompareCitiesInteractor.java
+++ src/main/java/use_case/note/CompareCities/CompareCitiesInteractor.java
@@ -1,18 +1,22 @@
 package use_case.note.CompareCities;
+
 import entity.Weather;
+
 /*
 * The Comparecities Interactor.
  */
-public class CompareCityiesnteractor implements CompareCitiesInputBoundary {
+public class CompareCitiesInteractor implements CompareCitiesInputBoundary {
     private final CompareCitiesDataAccessInterface compareCitiesDataAccessInterface;
     private final CompareCitiesOutputBoundary comparecitiesPresenter;
 
-    public CompareCityiesnteractor(CompareCitiesDataAccessInterface compareCitiesDataAccessInterface,
+    public CompareCitiesInteractor(CompareCitiesDataAccessInterface compareCitiesDataAccessInterface,
                                    CompareCitiesOutputBoundary comparecitiesPresenter) {
         this.comparecitiesPresenter = comparecitiesPresenter;
         this.compareCitiesDataAccessInterface = compareCitiesDataAccessInterface;
     }
 
+    // when execute by controller, call presenter to prepare fail or success view. If success, make a new a outputdata
+    // and put it into presenter
     public void execute(CompareCitiesInputData compareCitiesInputData) {
         final String firstcityname = compareCitiesInputData.getFirstcityname();
         final String secondcityname = compareCitiesInputData.getSecondcityname();
@@ -25,9 +29,9 @@ public class CompareCityiesnteractor implements CompareCitiesInputBoundary {
                 comparecitiesPresenter.prepareFailView("city not found");
             }
             else {
-              final Weather firstweather = compareCitiesDataAccessInterface.getWeather(firstcityname);
-              final Weather secondweather = compareCitiesDataAccessInterface.getWeather(secondcityname);
-              final CompareCitiesOutPutData compareCitiesOutPutData = new CompareCitiesOutPutData(firstweather, secondweather, false);
+                final Weather firstweather = compareCitiesDataAccessInterface.getWeather(firstcityname);
+                final Weather secondweather = compareCitiesDataAccessInterface.getWeather(secondcityname);
+                final CompareCitiesOutPutData compareCitiesOutPutData = new CompareCitiesOutPutData(firstweather, secondweather, false);
                 comparecitiesPresenter.prepareSuccessView(compareCitiesOutPutData);
             }
         }
