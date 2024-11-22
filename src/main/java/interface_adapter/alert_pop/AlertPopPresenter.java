578a4eb1ef456faf71762cc2b81217a935615c45
Annie Bu <annie.bu@mail.utoronto.ca>
2024-11-21 20:22:30 -0500

    deleted some use cases

---
+++ src/main/java/interface_adapter/alert_pop/AlertPopPresenter.java
@@ -0,0 +1,8 @@
+package interface_adapter.alert_pop;
+
+import interface_adapter.weather.WeatherViewModel;
+import use_case.note.alert_pop.AlertPopOutputBoundary;
+
+public class AlertPopPresenter implements AlertPopOutputBoundary {
+    private final WeatherViewModel
+}
--- src/main/java/use_case/note/close_pop/ClosePopOutputBoundary.java
+++
@@ -1,8 +0,0 @@
-package use_case.note.close_pop;
-
-public interface ClosePopOutputBoundary {
-
-    void presentSuccessView(ClosePopOutputData closePopOutputData);
-
-    void prepareFailView(String errorMessage);
-}
