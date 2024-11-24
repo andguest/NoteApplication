d9201325c77d17b8065bf13ac14110cf5cdca63c
ctrluserh <harman.saini@mail.utoronto.ca>
2024-11-24 16:41:12 -0500

    Completed the ConvertInteractorTest file to ensure 100% code coverage.

---
+++ src/test/java/use_case/note/ConvertInteractorTest.java
@@ -0,0 +1,117 @@
+package use_case.note;
+
+import data_access.WeatherDataAccessObject;
+import entity.Weather;
+import org.junit.Test;
+import use_case.note.convert_farenheit.ConvertFarenheitInputData;
+import use_case.note.convert_farenheit.ConvertFarenheitOutputBoundary;
+import use_case.note.convert_farenheit.ConvertFarenheitOutputData;
+import use_case.note.convert_farenheit.ConvertInteractor;
+
+import java.io.DataOutput;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ConvertInteractorTest {
+
+    /**
+     * Checks Lines 1 - 14.
+     */
+    @Test
+    public void TestIfWeatherDNE() {
+        WeatherDataAccessInterface Dao = new WeatherDataAccessInterface() {
+            @Override
+            public Weather getWeather(String city) throws IOException {
+                return WeatherDataAccessObject.getWeather(city);
+            }
+        };
+        ConvertFarenheitOutputBoundary boundary = new ConvertFarenheitOutputBoundary() {
+            @Override
+            public void prepareFailView(String errorMessage) {
+                assertEquals(errorMessage,"UNABLE TO CONVERT");
+            }
+
+            @Override
+            public void prepareSuccessView(ConvertFarenheitOutputData outputData) {
+                fail("this is unexpected");
+            }
+        };
+        Weather weather = Dao.getWeather("trn");
+
+        ConvertFarenheitInputData inputData = new ConvertFarenheitInputData(weather);
+
+        ConvertInteractor interactor = new ConvertInteractor(boundary);
+        interactor.executeConvert(inputData);
+    }
+
+    /**
+     * Convert to Fahrenheit.
+     * Checks Lines 28 - 36 and lines 15 - 22.
+     */
+    @Test
+    public void TestIfMetricConversion() {
+        WeatherDataAccessInterface Dao = new WeatherDataAccessInterface() {
+            @Override
+            public Weather getWeather(String city) throws IOException {
+                return WeatherDataAccessObject.getWeather(city);
+            }
+        };
+        ConvertFarenheitOutputBoundary boundary = new ConvertFarenheitOutputBoundary() {
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail(errorMessage);
+            }
+
+            @Override
+            public void prepareSuccessView(ConvertFarenheitOutputData outputData) {
+                assertEquals(false, outputData.getWeather().isMetric());
+
+            }
+        };
+
+        Weather weather = Dao.getWeather("Toronto");
+
+        ConvertFarenheitInputData inputData = new ConvertFarenheitInputData(weather);
+
+        ConvertInteractor interactor = new ConvertInteractor(boundary);
+        interactor.executeConvert(inputData);
+
+
+    }
+
+    /**
+     * Convert to Metric.
+     * Checks Lines 37 - end and lines 15 - 22.
+     */
+    @Test
+    public void TestIfFahrenheitConversion() {
+        WeatherDataAccessInterface Dao = new WeatherDataAccessInterface() {
+            @Override
+            public Weather getWeather(String city) throws IOException {
+                return WeatherDataAccessObject.getWeather(city);
+            }
+        };
+        ConvertFarenheitOutputBoundary boundary = new ConvertFarenheitOutputBoundary() {
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail(errorMessage);
+            }
+
+            @Override
+            public void prepareSuccessView(ConvertFarenheitOutputData outputData) {
+                assertTrue(outputData.getWeather().isMetric());
+
+            }
+        };
+
+        Weather weather = Dao.getWeather("Toronto");
+        weather.setMetric(false);
+
+        ConvertFarenheitInputData inputData = new ConvertFarenheitInputData(weather);
+
+        ConvertInteractor interactor = new ConvertInteractor(boundary);
+        interactor.executeConvert(inputData);
+
+    }
+}
