9e86e4aaa8dc5832be8a631a02aed150584f7157
Ishayu Sharma <ishayu.sharma@mail.utoronto.ca>
2024-11-28 17:10:49 -0500

    Added setPosition method to MapImagePanel

--- src/main/java/view/MapImagepanel.java
+++ src/main/java/view/MapImagepanel.java
@@ -12,15 +12,14 @@ import javax.swing.*;
 * the MapImagePanel is responsible for displaying the map file.
  */
 public class MapImagepanel extends JPanel {
-    private static final int ZOOM_VALUE = 7;
+    private static final int ZOOM_VALUE = 15;
     private static final int NUM_THREADS = 8;
 
-    private double[] coords;
+    private GeoPosition position;
     private JXMapViewer mapViewer;
 
     // Generates a JXMapViewer object given latitude and longitude
     public MapImagepanel(double latitude, double longitude) {
-        this.coords = new double[] {latitude, longitude};
         this.mapViewer = new JXMapViewer();
 
         final TileFactoryInfo info = new OSMTileFactoryInfo();
@@ -30,10 +29,20 @@ public class MapImagepanel extends JPanel {
 
         tileFactory.setThreadPoolSize(NUM_THREADS);
 
-        final GeoPosition position = new GeoPosition(this.coords);
+        this.position = new GeoPosition(latitude, longitude);
 
         this.mapViewer.setZoom(ZOOM_VALUE);
         mapViewer.setAddressLocation(position);
+    }
+
+    /**
+     * Changes the map's position.
+     * @param latitude the latitude of the new position
+     * @param longitude the longitude of the new position
+     */
+    public void setPosition(double latitude, double longitude) {
+        this.position = new GeoPosition(latitude, longitude);
+        this.mapViewer.setAddressLocation(position);
     }
 
     public JXMapViewer getDisplayfield() {
